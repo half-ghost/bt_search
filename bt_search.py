7dd17bc244016653e6f63f71520e56bf410baf53
half-ghost <55418764+half-ghost@users.noreply.github.com>
2022-06-30 16:53:56 +0800

    Update bt_search.py

--- bt_search.py
+++ bt_search.py
@@ -5,10 +5,13 @@ help_text = f'''[搜磁力numtag]:num为指定的引擎，目前有3个，tag为
 from bs4 import BeautifulSoup as bs
 import asyncio
 from hoshino import Service,aiorequests
+import math
+import random
+import json
 
 ciliwang = {
     "磁力猫":"https://clm182.xyz/",
-    "搜磁力":"https://soucili.info", #未完成
+    "搜磁力":"https://soucili.info",
     "种子搜":"https://m.zhongziso19.xyz/",
     "磁力蜘蛛":"http://www.eclzz.win"
 }
@@ -127,6 +130,47 @@ async def clzz_crawler(tag):
     else:
         return title_list, magnet_list, info_list, url
 
+async def soucili(tag):
+    soucilihead = {
+        'Accept': "application/json, text/javascript, */*; q=0.01",
+        'X-Requested-With': "XMLHttpRequest",
+        'Referer': 'https://soucili.info/'
+    }
+    def id():
+        a = hex(math.floor((1 + random.random()) * 65536))[3:]
+        return a
+    def calculate_size(size):
+        def strofsize(integer, remainder, level):
+            if integer >= 1024:
+                remainder = integer % 1024
+                integer //= 1024
+                level += 1
+                return strofsize(integer, remainder, level)
+            else:
+                return integer, remainder, level
+        units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+        integer, remainder, level = strofsize(size, 0, 0)
+        if level+1 > len(units):
+            level = -1
+        return ( '{}.{:>03d} {}'.format(integer, remainder, units[level]) )
+    device_id = f"{id()}{id()}-{id()}-{id()}-{id()}-{id()}{id()}{id()}"
+    url = ciliwang.get("搜磁力")
+    geturl = await aiorequests.get(f"{url}/request/?request_type=esearch&keywords={tag}&device_id={device_id}&page=0", headers=soucilihead)
+    if geturl.status_code != 200:
+        stat = "bad"
+        return stat
+    resp = await geturl.text
+    json_page = json.loads(resp)
+    title_list, magnet_list, info_list = [],[],[]
+    if json_page['videos'] == "":
+        return []
+    for i in json_page['videos']:
+        title_list.append(i['file_name'].replace(tag, f"『{tag}』"))
+        magnet_list.append("magnet:?xt=urn:btih:" + i['moc'])
+        info_list.append(f"大小:{calculate_size(i['file_size'])} 添加时间:{i['create_time']} 热度:{i['hot']}")
+
+    return title_list, magnet_list, info_list, url
+
 def mes_creater(data):
     title_list, magnet_list, info_list, url = data
     mes_list = []
@@ -153,7 +197,7 @@ def mes_creater(data):
 async def engine_search(tag, engine=None, All=False):
     if All:
         result = []
-        result_list = await asyncio.gather(clm_crawler(tag),zzs_crawler(tag),clzz_crawler(tag))
+        result_list = await asyncio.gather(clm_crawler(tag),zzs_crawler(tag),clzz_crawler(tag),soucili(tag))
         for i in result_list:
             if len(i) == 0 or i == "bad":
                 continue
@@ -166,6 +210,8 @@ async def engine_search(tag, engine=None, All=False):
         result = await zzs_crawler(tag)
     elif engine == "3":
         result = await clzz_crawler(tag)
+    elif engine == "4":
+        result = await soucili(tag)
     if result == "bad":
         return "该引擎访问失败，请更换引擎重试"
     if len(result) == 0:
@@ -193,8 +239,8 @@ async def gather_search(bot, ev):
 async def single_search(bot, ev):
     mes = ev.raw_message.replace("搜磁力", "")
     engine = mes[:1]
-    if engine not in "123":
-        await bot.send(ev, "请指定搜索引擎(目前有3个引擎)")
+    if engine not in "1234":
+        await bot.send(ev, "请指定搜索引擎(目前有4个引擎)")
         return
     tag = mes[1:].strip()
     await bot.send(ev, "请等待搜索结果")
